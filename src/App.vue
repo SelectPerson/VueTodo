e11e035c1b4aebdef3e80689b5324495042d0463
SelctPerson <test@gmail.com>
2020-05-25 11:50:39 +0300

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello World</h1>
+    <hr>
+    <br>
+    <AddTodo
+            @add-todo="addTodo"
+    />
+    <br>
+    <TodoList
+            v-bind:todos="TodoListArr"
+            v-on:remove-todo="removeTodo"
+    />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import TodoList from './components/TodoList.vue';
+  import AddTodo from './components/addTodo';
+  export default {
+    name: 'App',
+    components: {
+      TodoList,
+      AddTodo
+    },
+    methods: {
+      removeTodo(id) {
+        this.TodoListArr = this.TodoListArr.filter(t => t.id !== id)
+      },
+      addTodo(todo) {
+        this.TodoListArr.push(todo);
+        console.log('Vova: ', todo);
+      }
+    },
+    data() {
+      return {
+        TodoListArr: [
+          {
+            id: 1,
+            task: 'Learn Vue',
+            completed: false,
+          },
+          {
+            id: 2,
+            task: 'Hello World',
+            completed: false,
+          },
+          {
+            id: 3,
+            task: 'My Friend is the best',
+            completed: false,
+          }
+        ]
+      }
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  ul {
+    list-style: none;
+  }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ul>
+            <TodoItem
+                v-for="todoElem of todos"
+                v-bind:todo="todoElem"
+                v-on:remove-todo="removeTodo"
+            />
+        </ul>
+    </div>
+</template>
+
+<script>
+    import TodoItem from "./TodoItem";
+    export default {
+        props: ['todos'],
+        components: {
+            TodoItem
+        },
+        methods: {
+            removeTodo(id) {
+                this.$emit('remove-todo', id);
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
